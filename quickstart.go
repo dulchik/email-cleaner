065f1c1989cc182122a0b68c467ea6852573c061
dulchik <bubnyukab@gmail.com>
2025-12-08 10:51:43 +0100

    parses first 50 emails and creates a simple html table

--- quickstart.go
+++ quickstart.go
@@ -14,6 +14,12 @@ import (
 	"google.golang.org/api/option"
 )
 
+type Email struct {
+	From 		string
+	Subject 	string
+	Unsubscribe string
+}
+
 // Retrieve a token, saves the token, then returns the generated client.
 func getClient(config *oauth2.Config) *http.Client {
 	// The file token.json stores the user's access and refresh tokens, and is
@@ -30,16 +36,30 @@ func getClient(config *oauth2.Config) *http.Client {
 
 // Request a token from the web, then returns the retrieved token.
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	// start temporary server
+	codeCh := make(chan string)
+	srv := &http.Server{Addr: ":8080"}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+		fmt.Fprintf(w, "Authorization successful! You can close this window.")
+		codeCh <- code
+		go srv.Shutdown(context.Background())
+	})
+
+	go func() {
+		log.Println("Starting local server on http://localhost:8080/")
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Auth server error: %v", err)
+		}
+	}()
+
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
 	fmt.Printf("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
+	code := <-codeCh
+	tok, err := config.Exchange(context.Background(), code)
 	if err != nil {
 		log.Fatalf("Unable to retrieve token from web: %v", err)
 	}
@@ -77,10 +97,12 @@ func main() {
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, gmail.GmailReadonlyScope)
+	config, err := google.ConfigFromJSON(b, gmail.GmailModifyScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
+	config.RedirectURL = "http://localhost:8080/"
+
 	client := getClient(config)
 
 	srv, err := gmail.NewService(ctx, option.WithHTTPClient(client))
@@ -89,16 +111,49 @@ func main() {
 	}
 
 	user := "me"
-	r, err := srv.Users.Labels.List(user).Do()
+	r, err := srv.Users.Messages.List(user).LabelIds("INBOX").MaxResults(50).Do()
 	if err != nil {
-		log.Fatalf("Unable to retrieve labels: %v", err)
+		log.Fatalf("Unable to retrieve messages: %v", err)
 	}
-	if len(r.Labels) == 0 {
+	if len(r.Messages) == 0 {
 		fmt.Println("No labels found.")
 		return
 	}
-	fmt.Println("Labels:")
-	for _, l := range r.Labels {
-		fmt.Printf("- %s\n", l.Name)
+
+	emails := []Email{}
+	for _, m := range r.Messages {
+		msg, err := srv.Users.Messages.Get(user, m.Id).Format("metadata").Do()
+		if err != nil {
+			log.Printf("Unable to get message %s: %v", m.Id, err)
+			continue
+		}
+
+		var from, subject, unsubscribe string
+		for _, h := range msg.Payload.Headers {
+			switch h.Name {
+			case "From":
+				from = h.Value
+			case "Subject":
+				subject = h.Value
+			case "List-Unsubscribe":
+				unsubscribe = h.Value
+			}
+		}
+		emails = append(emails, Email{
+			From: from,
+			Subject: subject,
+			Unsubscribe: unsubscribe,
+		})
 	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+    	fmt.Fprintln(w, "<table border='1'><tr><th>From</th><th>Subject</th><th>Unsubscribe</th></tr>")
+		for _, e := range emails {
+        	fmt.Fprintf(w, "<tr><td>%s</td><td>%s</td><td>%s</td></tr>", e.From, e.Subject, e.Unsubscribe)
+    	}
+       	fmt.Fprintln(w, "</table>")
+   	})
+
+    log.Println("Listening on http://localhost:8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
 }
